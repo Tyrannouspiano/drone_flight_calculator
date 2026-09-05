ca5bba0f39ba35a26e49d6ecec354f50e8a80149
Justin M <meyej1@farmingdale.edu>
2026-09-04 20:16:42 -0400

    Created the function flight_time_table

--- flight_calculator.py
+++ flight_calculator.py
@@ -9,3 +9,13 @@ def calculate_flight_time(weight_grams):
         raise ValueError("Flight time cannot be negative")
     return flight_time
     #These 2 if statements were accepted
+
+#Returns a list of (weight, flight_time) pairs for payload weights from 0 to max_weight_grams in steps of step_grams, calling
+#calculate_flight_time for each weight
+def flight_time_table(max_weight_grams, step_grams):
+    table = []
+    for weight in range(0, max_weight_grams + 1, step_grams):
+        flight_time = calculate_flight_time(weight)
+        table.append((weight, flight_time))
+    return table
+#This function was accepted as is
